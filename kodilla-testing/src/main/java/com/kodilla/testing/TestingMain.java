d3aa8e6d5b643968201d7d76b6e3ac9922d7312f
Valdrab <mathez@wp.pl>
2019-10-09 18:27:23 +0200

    Pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,42 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - Calculator class - adding:");
+
+        Calculator calculator = new Calculator();
+
+        int resultOfAdding = calculator.add(12, 80);
+        int expectedResultOfAdding = 92;
+
+        if (resultOfAdding == expectedResultOfAdding) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - Calculator class - subtracting:");
+
+        int resultOfSubtracting = calculator.subtract(20, 5);
+        int expectedResultOfSubtracting = 15;
+
+        if (resultOfSubtracting == expectedResultOfSubtracting) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
