9d9c67966077e13a3f78db6799674ac7fc84aae7
Valdrab <mathez@wp.pl>
2019-10-30 12:48:38 +0100

    Zadanie 7.4

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -0,0 +1,21 @@
+package com.kodilla.stream.world;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class World {
+    private final List<Continent> listOfContinents = new ArrayList<>();
+
+    public boolean addContinent(Continent continent){
+        return listOfContinents.add(continent);
+    }
+
+    public BigDecimal getPeopleQuantity(){
+            BigDecimal worldPeopleQuantity = listOfContinents.stream()
+                .flatMap(continent -> continent.getListOfCountries().stream())
+                .map(country -> country.getPeopleQuantity())
+                .reduce(BigDecimal.ZERO, (sum, current) -> sum.add(current));
+            return worldPeopleQuantity;
+    }
+}
