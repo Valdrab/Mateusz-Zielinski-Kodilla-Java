4ad40c77e32240265a5b9d0cde2b2b1fa486d5ce
Valdrab <mathez@wp.pl>
2019-10-30 13:14:17 +0100

    Zadanie 7.2 - rozszerzenie

--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -1,12 +1,19 @@
 package com.kodilla.stream.immutable;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 public final class ForumUser {
     private final String username;
     private final String realName;
+    private final Date birthDate;
+    private final List<ForumUser> friends = new ArrayList<>();
 
-    public ForumUser(final String username, final String realName) {
+    public ForumUser(final String username, final String realName, final Date birthDate) {
         this.username = username;
         this.realName = realName;
+        this.birthDate = birthDate;
     }
 
     public String getUsername() {
@@ -15,5 +22,17 @@ public final class ForumUser {
 
     public String getRealName() {
         return realName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public boolean addFriend(ForumUser forumUser) {
+        return friends.add(forumUser);
+    }
+
+    public boolean removeFriend(ForumUser forumUser) {
+        return friends.remove(forumUser);
     }
 }
