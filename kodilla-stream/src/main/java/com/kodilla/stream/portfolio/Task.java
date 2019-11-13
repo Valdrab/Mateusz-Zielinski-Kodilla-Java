fcb1e043035e6277113704afc7b25117739a67e2
Valdrab <mathez@wp.pl>
2019-11-13 17:55:23 +0100

    Zadanie 7.6

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/portfolio/Task.java
@@ -0,0 +1,69 @@
+package com.kodilla.stream.portfolio;
+
+import java.time.LocalDate;
+
+public final class Task {
+    private final String title;
+    private final String description;
+    private final User assignedUser;
+    private final User creator;
+    private final LocalDate created;
+    private final LocalDate deadline;
+
+    public Task(final String title, final String description,
+                final User assignedUser, final User creator,
+                final LocalDate created, final LocalDate deadline) {
+        this.title = title;
+        this.description = description;
+        this.assignedUser = assignedUser;
+        this.creator = creator;
+        this.created = created;
+        this.deadline = deadline;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public User getAssignedUser() {
+        return assignedUser;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public LocalDate getCreated() {
+        return created;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", assignedUser=" + assignedUser +
+                ", creator=" + creator +
+                ", created=" + created +
+                ", deadline=" + deadline +
+                '}' + "\n";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Task task = (Task) o;
+
+        return title.equals(task.title);
+    }
+}
