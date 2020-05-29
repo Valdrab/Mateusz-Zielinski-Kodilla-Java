82d8e7b1ae3d509a2245a56edd158cd73a1c4027
Valdrab <mathez@wp.pl>
2020-05-29 15:18:48 +0200

    [15.4 - added not_null assertions]

--- kodilla-patterns/src/test/java/com/kodilla/patterns/factory/tasks/TaskFactoryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/factory/tasks/TaskFactoryTestSuite.java
@@ -10,6 +10,7 @@ public class TaskFactoryTestSuite {
         TaskFactory factory = new TaskFactory();
         //When
         Task shopping = factory.createTask(TaskFactory.SHOPPING);
+        assert shopping != null;
         shopping.executeTask();
         //Then
         Assert.assertEquals("Quick shopping", shopping.getTaskName());
@@ -22,6 +23,7 @@ public class TaskFactoryTestSuite {
         TaskFactory factory = new TaskFactory();
         //When
         Task painting = factory.createTask(TaskFactory.PAINTING);
+        assert painting != null;
         painting.executeTask();
         //Then
         Assert.assertEquals("Household painting", painting.getTaskName());
@@ -34,6 +36,7 @@ public class TaskFactoryTestSuite {
         TaskFactory factory = new TaskFactory();
         //When
         Task driving = factory.createTask(TaskFactory.DRIVING);
+        assert driving != null;
         driving.executeTask();
         //Then
         Assert.assertEquals("Joyride", driving.getTaskName());
